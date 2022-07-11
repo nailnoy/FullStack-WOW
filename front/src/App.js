d0aebc0305aa313f1837580a402c691969db4109
nailboy <omomo010@gmail.com>
2022-07-11 17:16:36 +0900

    환경설정

--- front/src/App.js
+++ front/src/App.js
@@ -1,26 +1,11 @@
-import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-
-  const [message, setMessage] = useState("");
-
-  useEffect(() => {
-    fetch('/api/hello')
-      .then(response => response.text())
-      .then(message => {
-        setMessage(message);
-      });
-  },[])
-
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {message}
-        </p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
