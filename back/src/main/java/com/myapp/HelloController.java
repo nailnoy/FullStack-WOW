d8515d3a84318881028fcf2e461364eb5f065e6d
nailboy <omomo010@gmail.com>
2022-07-11 17:47:40 +0900

    간단한 비동기 테스트

--- back/src/main/java/com/myapp/HelloController.java
+++ back/src/main/java/com/myapp/HelloController.java
@@ -3,12 +3,13 @@ package com.myapp;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Date;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 public class HelloController {
-    @GetMapping("/api/hello")
-    public String hello() {
-        return "야 이거 가져오냐 ? " + new Date() + "\n";
-    }
+	  @GetMapping("hello")
+	    public List<String> hello() {
+	        return Arrays.asList("안녕하세요", "Hello");
+	    }
 }
