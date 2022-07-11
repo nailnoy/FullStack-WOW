107e4ff10001dc060809f478bad7e24791f33d44
nailboy <omomo010@gmail.com>
2022-07-11 18:44:29 +0900

    기초 구현

--- front/src/App.js
+++ front/src/App.js
@@ -1,41 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-import {useEffect, useState} from "react";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App() {
-    const [message, setMessage] = useState([]);
+import GlobalStyles from "./GlobalStyles";
 
-    useEffect(() => {
-        fetch("/hello")
-            .then((response) => {
-                return response.json();
-            })
-            .then(function (data) {
-                setMessage(data);
-            });
-    }, []);
-
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-                <ul>
-                    {message.map((text, index) => <li key={`${index}-${text}`}>{text}</li>)}
-                </ul>
-            </header>
-        </div>
-    );
-}
+const App = () => {
+	return (
+		<BrowserRouter>
+			<GlobalStyles />
+				<Routes>
+					<Route exact path="/" />
+					<Route exact path="/board" />
+					<Route exact path="/detail/:id"  />
+					<Route exact path="/myPage"  />
+				</Routes>
+		</BrowserRouter>
+	);
+};
 
 export default App;
