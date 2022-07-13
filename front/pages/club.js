c2d7bf23aabd382969b0e86e7d65066aa4dc170a
Zolyer <tannaim@kyonggi.ac.kr>
2022-07-13 17:43:24 +0900

    Create club.js

---
+++ front/pages/club.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Main from "../components/clubPage/Main";
+import Nav from "../components/Nav";
+import Footer from "../components/Footer";
+
+const club = () => {
+    return(
+        <>
+            <Nav />
+            <Main />
+            <Footer />
+        </>
+    );
+};
+
+export default club;
