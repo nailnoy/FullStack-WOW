12a0729ea437a890b14b64774208f13b54ee9554
Zolyer <tannaim@kyonggi.ac.kr>
2022-07-15 20:35:07 +0900

    Update Card

--- front/src/components/clubPage/ClubCard.js
+++ front/src/components/clubPage/ClubCard.js
@@ -7,47 +7,125 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import Box from "@mui/material/Box";
 
 const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 const ClubCard = () => {
-    return(
-        <Container sx={{ py: 8 }} maxWidth="md">
-            <Typography gutterBottom variant="h6" component="h2" fontFamily="Jua" fontSize="2rem">
-            등록된 모임 목록
-            </Typography>
-            <Grid container spacing={4}>
-                {cards.map((card) => (
-                    <Grid item key={card} xs={12} sm={6} md={4}>
-                        <Card
-                            sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
-                            >
-                        <CardMedia
-                            component="img"
-                            sx={{
-                                pt: '10%',
-                            }}
-                            image="https://velog.velcdn.com/images/zolyer/post/d8620848-232a-47c5-a6db-2283b9fe4d28/image.jpeg"
-                            alt="default"
-                            />
-                        <CardContent sx={{ flexGrow: 1 }}>
-                            <Typography gutterBottom variant="h5" component="h2" fontFamily="Jua" fontWeight="600">
-                                모집 제목
-                            </Typography>
-                            <Typography fontFamily="Jua">
-                                간략한 소개문입니다.
-                            </Typography>
-                        </CardContent>
-                        <CardActions>
-                            <Button size="small"variant="outlined">자세히</Button>
-                            <Button size="small" variant="outlined" color="success">참가 신청</Button>
-                        </CardActions>
-                        </Card>
-                    </Grid>
-                ))}
-            </Grid>
-        </Container>
-    )
-}
+  return (
+    <Container sx={{ py: 0 }} maxWidth="md">
+      <Grid container spacing={4}>
+        {cards.map((card) => (
+          <Grid item key={card} xs={12} sm={6} md={4}>
+            <Card
+              raised
+              sx={{
+                width: "md",
+                margin: "0 auto",
+                padding: "0.1em",
+                position: "relative",
+              }}
+            >
+                <Grid 
+                  container spacing={0}
+                  justifyContent="space-between"
+                  sx={{
+                    position: "absolute",
+                    left: 0,
+                    width: "100%",
+                    pt: "270px",
+                    px: "20px",
+                    m: "0 auto",
+                  }}
+                >
+                  <Box 
+                    sx={{
+                      textAlign: "center",
+                      bgcolor: "coral",
+                      boxShadow: 1,
+                      borderRadius: 2,
+                      p: 1,
+                      minWidth: 45,
+                      maxHeight: 35,
+                    }}
+                  >
+                      태그
+                  </Box>
+                  <Box 
+                    sx={{
+                      textAlign: "center",
+                      bgcolor: "red",
+                      boxShadow: 1,
+                      borderRadius: 2,
+                      p: 1,
+                      minWidth: 45,
+                      maxHeight: 35,
+                      
+                    }}
+                  >
+                    마감
+                  </Box>
+                </Grid>
+              <CardMedia
+                component="img"
+                sx={{
+                  padding: "0.5em 0.5em 0 0.5em",
+                  objectFit: "contain",
+                }}
+                height="300"
+                image="https://velog.velcdn.com/images/zolyer/post/d8620848-232a-47c5-a6db-2283b9fe4d28/image.jpeg"
+                // image="https://p4.wallpaperbetter.com/wallpaper/41/681/303/pc-hd-1080p-nature-1920x1080-wallpaper-preview.jpg"
+                alt="default"
+              />
+              <CardContent sx={{ flexGrow: 1 }}>
+              <Typography
+                  gutterBottom
+                  variant="h5"
+                  fontFamily="Jua"
+                  fontWeight="600"
+                  sx={{
+                    overflow: "hidden",
+                    textOverflow: "ellipsis",
+                    display: "-webkit-box",
+                    WebkitLineClamp: '1',
+                    WebkitBoxOrient: 'vertical',
+                  }}
+                >
+                  모집 제목
+                </Typography>
+                <Typography
+                  fontFamily="Jua"
+                  height="70px"
+                  sx={{
+                    overflow: "hidden",
+                    textOverflow: "ellipsis",
+                    display: "-webkit-box",
+                    WebkitLineClamp: '3',
+                    WebkitBoxOrient: 'vertical',
+                  }}
+                >
+                  간략한 소개문입니다.
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small" variant="outlined">
+                  자세히
+                </Button>
+                <Button size="small" variant="outlined" color="success">
+                  참가 신청
+                </Button>
+                <IconButton aria-label="add to favorites">
+                  <FavoriteIcon />
+                </IconButton>
+              </CardActions>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  );
+};
 
 export default ClubCard;
--- front/src/components/myPage/LikedClubCard.js
+++ front/src/components/myPage/LikedClubCard.js
@@ -9,12 +9,7 @@ import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import FavoriteIcon from '@mui/icons-material/Favorite';
-import styled from "styled-components";
-// import { customMedia } from "../../GlobalStyles";
-
-import SmallTag from "../common/SmallTag";
-import ExpiredTag from "../common/ExpiredTag";
-// import WaitingTag from "../common/WaitingTag";
+import Box from "@mui/material/Box";
 
 const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
@@ -25,103 +20,112 @@ const LikedClubCard = () => {
         {cards.map((card) => (
           <Grid item key={card} xs={12} sm={6} md={4}>
             <Card
-              sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+              raised
+              sx={{
+                width: "md",
+                margin: "0 auto",
+                padding: "0.1em",
+                position: "relative",
+              }}
             >
-
+                <Grid 
+                  container spacing={0}
+                  justifyContent="space-between"
+                  sx={{
+                    position: "absolute",
+                    left: 0,
+                    width: "100%",
+                    pt: "270px",
+                    px: "20px",
+                    m: "0 auto",
+                  }}
+                >
+                  <Box 
+                    sx={{
+                      textAlign: "center",
+                      bgcolor: "coral",
+                      boxShadow: 1,
+                      borderRadius: 2,
+                      p: 1,
+                      minWidth: 45,
+                      maxHeight: 35,
+                    }}
+                  >
+                      태그
+                  </Box>
+                  <Box 
+                    sx={{
+                      textAlign: "center",
+                      bgcolor: "red",
+                      boxShadow: 1,
+                      borderRadius: 2,
+                      p: 1,
+                      minWidth: 45,
+                      maxHeight: 35,
+                      
+                    }}
+                  >
+                    마감
+                  </Box>
+                </Grid>
               <CardMedia
                 component="img"
                 sx={{
-                  pt: '10%',
+                  padding: "0.5em 0.5em 0 0.5em",
+                  objectFit: "contain",
                 }}
+                height="300"
                 image="https://velog.velcdn.com/images/zolyer/post/d8620848-232a-47c5-a6db-2283b9fe4d28/image.jpeg"
+                // image="https://p4.wallpaperbetter.com/wallpaper/41/681/303/pc-hd-1080p-nature-1920x1080-wallpaper-preview.jpg"
                 alt="default"
               />
-              <TagContainer>
-                <ClubTag>온라인</ClubTag>
-              </TagContainer>
-              <>
-                  <ClubExpiredTag>마감</ClubExpiredTag>
-                </>
               <CardContent sx={{ flexGrow: 1 }}>
-                <Typography gutterBottom variant="h5" component="h2" fontFamily="Jua" fontWeight="600">
+              <Typography
+                  gutterBottom
+                  variant="h5"
+                  fontFamily="Jua"
+                  fontWeight="600"
+                  sx={{
+                    overflow: "hidden",
+                    textOverflow: "ellipsis",
+                    display: "-webkit-box",
+                    WebkitLineClamp: '1',
+                    WebkitBoxOrient: 'vertical',
+                  }}
+                >
                   모집 제목
                 </Typography>
-                <Typography fontFamily="Jua">
+                <Typography
+                  fontFamily="Jua"
+                  height="70px"
+                  sx={{
+                    overflow: "hidden",
+                    textOverflow: "ellipsis",
+                    display: "-webkit-box",
+                    WebkitLineClamp: '3',
+                    WebkitBoxOrient: 'vertical',
+                  }}
+                >
                   간략한 소개문입니다.
                 </Typography>
               </CardContent>
               <CardActions>
-                <Button size="small" variant="outlined">자세히</Button>
-                <Button size="small" variant="outlined" color="success">참가 신청</Button>
+                <Button size="small" variant="outlined">
+                  자세히
+                </Button>
+                <Button size="small" variant="outlined" color="success">
+                  참가 신청
+                </Button>
                 <IconButton aria-label="add to favorites">
                   <FavoriteIcon />
                 </IconButton>
               </CardActions>
-
             </Card>
-
           </Grid>
         ))}
       </Grid>
     </Container>
-
   );
 };
 
 export default LikedClubCard;
-
-
-const TagContainer = styled.div`
-	display: flex;
-	gap: 5px;
-
-	position: relative;
-  padding: 10px;
-  bottom: 60px;
-  
-`;
-
-const ClubTag = styled(SmallTag)`
-	& {
-		font-size: 14px;
-    padding: 7px 13px;
-	}
-`;
-
-// const LikeNum = styled.span`
-//   ${customMedia.lessThan("mobile")`
-//     font-size: 14px;
-//   `}
-
-//   ${customMedia.between("mobile", "largeMobile")`
-//     font-size: 16px;
-//   `}
-
-// 	${customMedia.between("largeMobile", "tablet")`
-//     font-size: 12px;
-//   `}
-
-// 	${customMedia.between("tablet", "desktop")`
-//     font-size: 12px;
-//   `}`;
-
-const ClubExpiredTag = styled(ExpiredTag)`
-	& {
-		font-size: 14px;
-		padding: 5px;
-		position: relative;
-    left: 75%;
-    bottom: 105px;
-	}
-`;
-
-// const ClubWaitingTag = styled(WaitingTag)`
-// 	& {
-// 		font-size: 14px;
-// 		padding: 5px;
-// 		position: relative;
-//     left: 75%;
-//     bottom: 105px;
-// 	}
-// `;
-
