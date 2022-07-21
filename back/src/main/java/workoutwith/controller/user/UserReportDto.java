17b84f2a3275d6113776525140d8c50923f0f0d3
Zolyer <tannaim@kyonggi.ac.kr>
2022-07-21 21:04:20 +0900

    권한 추가 (미완성)

--- back/src/main/java/workoutwith/controller/user/UserReportDto.java
+++ back/src/main/java/workoutwith/controller/user/UserReportDto.java
@@ -1,15 +1,19 @@
 package workoutwith.controller.user;
 
-import workoutwith.domain.User;
-import lombok.*;
 import org.springframework.beans.BeanUtils;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import workoutwith.domain.AuthorityStatus;
+import workoutwith.domain.User;
 
 @Getter
 @Setter
 @NoArgsConstructor
 public class UserReportDto { //DTO : 로직x. 순수한 데이터 객체, getter, setter 만을 가짐
 
-    private int authority;
+    private AuthorityStatus authority;
     private int declaration;
 
     public UserReportDto(User user) {
