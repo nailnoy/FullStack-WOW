86936c6a50a272356622dd932c88572e3e2b6a9c
Zolyer <tannaim@kyonggi.ac.kr>
2022-07-21 18:02:06 +0900

    엑세스키 yml 추가 및 gitignore

--- back/src/main/java/workoutwith/BackApplication.java
+++ back/src/main/java/workoutwith/BackApplication.java
@@ -10,7 +10,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class BackApplication {
 
     public static final String APPLICATION_LOCATIONS = "spring.config.location="
-            + "classpath:application.yml";
+            + "classpath:application.yml"
+            + ",classpath:s3key.yml";
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(BackApplication.class)
